f399d2fe575b4f04fafbd1b5dd0ee051aaa0731f
sara <saraelsadek55555@gmail.com>
2024-03-14 00:29:20 +0200

    sixth commit

--- src/compontents/Feed.jsx
+++ src/compontents/Feed.jsx
@@ -11,7 +11,7 @@ const Feed = () => {
    FetcFromAPI(`search?part=snippet&q=${selectedCategory}`)
     .then((data)=>{setVideos(data.items)})
 
-  },[selectedCategory])
+  },[selectedCategory]);  
   return (
     <Stack sx={{flexDirection:{sx:"column",md:"row"}}}>
       <Box sx={{height:{sx:'auto',md:'92vh'},borderRight:"1px solid #3d3d3d",px:{sx:0,md:2}}}>
