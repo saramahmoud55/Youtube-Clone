d15b295e940de9c93a1091a764eb800ebd6b3147
sara <saraelsadek55555@gmail.com>
2024-03-17 13:02:26 +0200

    channelDetail

--- src/compontents/ChannelCard.jsx
+++ src/compontents/ChannelCard.jsx
@@ -3,13 +3,17 @@ import { Box ,CardContent,CardMedia,Typography } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../Utils/constants'
-const ChannelCard = ({channelDetail}) => {
+const ChannelCard = ({channelDetail,marginTop}) => {
   return (
-    <Box sx={{boxShadow:'none',borderRadius:'20px',display:'flex',justifyContent:'center',alignItems:'center',width:{xs:'356px',md:'320px'},height:'326px',margin:'auto'}}>
+    <Box sx={{boxShadow:'none',borderRadius:'20px',
+    display:'flex',justifyContent:'center',
+    alignItems:'center',width:{xs:'356px',md:'320px'},height:'326px',margin:'auto',marginTop:marginTop}}>
      
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
        
-        <CardContent sx={{display:'flex',flexDirection:'column',justifyContent:'center',textAlign:'center',color:'#fff'}}>
+        <CardContent sx={{display:'flex',flexDirection:'column'
+        ,justifyContent:'center',textAlign:'center',
+        color:'#fff'}}>
           
           <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} 
           alt={channelDetail?.snippet?.title}
--- src/compontents/ChannelDetail.jsx
+++ src/compontents/ChannelDetail.jsx
@@ -1,13 +1,13 @@
 import {useState,useEffect} from 'react'
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
-import {Videos ,ChannelCard} from './' 
+import {Videos} from './Videos'
 import { FetcFromAPI } from '../Utils/FetchFromAPI'
+import ChannelCard from './ChannelCard';
 const ChannelDetail = () => {
   const {id} =useParams();
-  const [ChannelDetail,setChannelDetail]=useState(null)
+  const [channelDetail,setChannelDetail]=useState(null)
   const [videos,setVideos]=useState([])
-  console.log(ChannelDetail,videos)
   useEffect(()=>{
     FetcFromAPI(`channels?.part=snippet&id=${id}`)
     .then((data)=>setChannelDetail(data?.items[0]))
@@ -17,8 +17,11 @@ const ChannelDetail = () => {
 
   },[id])
   return (
-    <Box>
-      {id}
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{background: "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(121,9,85,1) 0%, rgba(0,212,255,1) 100%)",zIndex:10,height:'300px'}}/>
+      <ChannelCard channelDetail={channelDetail} marginTop='-93px'/>
+      </Box>
     </Box>
   )
 }
