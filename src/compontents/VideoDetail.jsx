ce2fe92562f23736b293b39ff0b9f0b9b5814ae6
sara <saraelsadek55555@gmail.com>
2024-03-19 13:03:48 +0200

    video detail

--- src/compontents/VideoDetail.jsx
+++ src/compontents/VideoDetail.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import { Link ,useParams} from "react-router-dom"
+import ReactPlayer from "react-player"
+import {Box,Stack,Typography} from '@mui/material'
+import { CheckCircle } from "@mui/icons-material"
+import {Videos} from './'
+import { FetcFromAPI } from "../Utils/FetchFromAPI"
 
 const VideoDetail = () => {
+  
+  const{id}=useParams();
+  const [VideoDetail,setVideoDetail]=useState(null);
+  useEffect(()=>{
+    FetcFromAPI(`videos?part=snippet,statistics&id=${id}`)
+    .then((data)=>setVideoDetail(data.items[0]))
+  },[id])
+
+
   return (
-    <div>
-      
-    </div>
+    <Box minHeight="95vh">
+      <Stack direction={{xs:'column',md:"row"}}>
+        <Box flex={1}>
+          <Box sx={{width:'100%',position:'sticky',top:'86px'}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} 
+            className="react-player" controls/>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
